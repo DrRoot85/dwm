e68fed3c3d7da2acef4eb5c76141b29023441772
DrRoot85 <dr.root85@gmail.com>
2022-07-16 21:04:34 +0300

    1

--- config.def.h
+++ config.def.h
@@ -191,8 +191,8 @@ static Key keys[] = {
 	{ 0, XF86LaunchA,		spawn,		{.v = (const char*[]){ "firefox", NULL } } },
 
 
-	{ 0, XF86KbdBrightnessUp,		spawn,		{.v = (const char*[]) {"brightnessctl", "--device='smc::kbd_backlight'" , "set", "+5""} } },
-	{ 0, XF86KbdBrightnessDown,		spawn,		{.v = (const char*[]) {"brightnessctl", "--device='smc::kbd_backlight'" , "set", "5-""} } },
+	{ 0, XF86KbdBrightnessUp,		spawn,		{.v = (const char*[]) {"brightnessctl", "--device='smc::kbd_backlight'" , "set", "+5"} } },
+	{ 0, XF86KbdBrightnessDown,		spawn,		{.v = (const char*[]) {"brightnessctl", "--device='smc::kbd_backlight'" , "set", "5-"} } },
 };
 
 /* button definitions */
---
+++ theme_beg.h
@@ -0,0 +1,3 @@
+# if defined DWM_BORDERPX || defined DWM_SNAP || defined DWM_SHOWBAR || defined DWM_TOPBAR || defined DWM_FONT || defined DWM_FOREGROUND || defined DWM_BACKGROUND || defined DWM_BORDER || defined DWM_SELFOREGROUND || defined DWM_SELBACKGROUND || defined DWM_SELBORDER || defined DWM_SELBORDER || 0
+# error (conflicting macro names)
+# endif
