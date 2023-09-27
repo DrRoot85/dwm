1a3a254071fdf96415955ca2b93536c1331deaec
DrRoot85 <mhbmis@icloud.com>
2023-09-28 02:46:39 +0300

    Last

--- config.def.h
+++ config.def.h
@@ -63,8 +63,8 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
